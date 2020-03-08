cae6011019b6c64be3295cbd78a1a83923d55524
Josh Everett <josheverett93@gmail.com>
2020-03-08 14:17:06 -0500

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,56 +4,41 @@ $(document).ready(function(){
 var dayAndTime = moment().format('LLLL');
 var container = $("#container");
 // var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-var workData = {"9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": ""};
+var workData = {"9": "", "10": "", "11": "", "12": "", "13": "", "14": "", "15": "", "16": "", "17": ""}[moment().hour()];
 var savedTasks = JSON.parse(localStorage.getItem("tasks")) || workData;
 
-var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
+// var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
 
 $("#currentDay").append(dayAndTime);
 
+function setColor(time) {
+  if (time === workData) {
+    return 'green'
+  }
+  if (time > workData) {
+    return 'red'
+  }
+  if (time < workData) {
+    return 'blue'
+  }
+}
+
 function createTimeBlocks() {
-    var blocks = "";
-    // for (var i = 0; i < workHours.length; i++) {
-    //     var hourBlock = "" +
-    //         '<div class="input-group mb-3">' +
-    //         '<div class="input-group-prepend">' +
-    //         '<span class="input-group-text">' + workHours[i] + '</span>' +
-    //         '</div>' +
-    //         '<input type="text" class="form-control" id="input-' + workHours[i] + '">' +
-    //         '<div class="input-group-append">' +
-    //         '<button class="btn btn-primary save-button" data-time="'  + workHours[i] + '"><i class="fa fa-save"></i></button>' +
-    //         '</div>' +
-    //         '</div>';
-    //     blocks += hourBlock;
-    // }
-
-    function setColor(time) {
-      if (time === currentSlot) {
-        return 'green'
-      }
-      if (time > currentSlot) {
-        return 'red'
-      }
-      if (time < currentSlot) {
-        return 'blue'
-      }
-    }
-
-    Object.keys(savedTasks).forEach(function(time) {
-      var hourBlock = "" +
-          '<div class="input-group mb-3">' +
-          '<div class="input-group-prepend">' +
-          '<span class="input-group-text">' + time + '</span>' +
-          '</div>' +
-          // '<input type="text" class="form-control" id="input-' + time + '">' + savedTasks[time] + '</input>' +
-          `<input type="text" style="background-color: ${setColor(time)}" class="form-control" id="input-${time}" value="${savedTasks[time]}"/>` +
-          '<div class="input-group-append">' +
-          '<button class="btn btn-primary save-button" data-time="'  + time + '"><i class="fa fa-save"></i></button>' +
-          '</div>' +
-          '</div>';
-      blocks += hourBlock;
-    });
-
+  Object.keys(savedTasks).forEach(function(time) {
+    var hourBlock = "" +
+        '<div class="input-group mb-3">' +
+        '<div class="input-group-prepend">' +
+        '<span class="input-group-text">' + time + '</span>' +
+        '</div>' +
+        // '<input type="text" class="form-control" id="input-' + time + '">' + savedTasks[time] + '</input>' +
+        `<input type="text" style="background-color: ${setColor(time)}" class="form-control" id="input-${time}" value="${savedTasks[time]}"/>` +
+        '<div class="input-group-append">' +
+        '<button class="btn btn-primary save-button" data-time="'  + time + '"><i class="fa fa-save"></i></button>' +
+        '</div>' +
+        '</div>';
+    blocks += hourBlock;
+  });
+    
     container.append(blocks);
 
     $(".save-button").on("click", function(event) {
@@ -78,6 +63,21 @@ console.log(savedTasks);
 
 
 })
+
+
+// for (var i = 0; i < workHours.length; i++) {
+    //     var hourBlock = "" +
+    //         '<div class="input-group mb-3">' +
+    //         '<div class="input-group-prepend">' +
+    //         '<span class="input-group-text">' + workHours[i] + '</span>' +
+    //         '</div>' +
+    //         '<input type="text" class="form-control" id="input-' + workHours[i] + '">' +
+    //         '<div class="input-group-append">' +
+    //         '<button class="btn btn-primary save-button" data-time="'  + workHours[i] + '"><i class="fa fa-save"></i></button>' +
+    //         '</div>' +
+    //         '</div>';
+    //     blocks += hourBlock;
+    // }
 
 // Display only standard working hours (i.e. 9 AM - 5 PM) and color code each block based on the current time. (Hint: HTML elements should be dynamically created.)
     // dynamically populate container with time blocks (grab container id to do this)

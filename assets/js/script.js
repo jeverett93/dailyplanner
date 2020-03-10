67d1fa6da9d69e281ec8a42c8d94934a9c91ab94
Josh Everett <josheverett93@gmail.com>
2020-03-09 20:01:32 -0500

    changing to military time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 // Getting current time to pop up
 var dayAndTime = moment().format('LLLL');
 var container = $("#container");
-// var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-var workData = {"9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": ""};
+var workData = {"9": "", "10": "", "11": "", "12": "", "13": "", "14": "", "15": "", "16": "", "17": ""};
 var savedTasks = JSON.parse(localStorage.getItem("tasks")) || workData;
 
 var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
@@ -13,19 +12,7 @@ $("#currentDay").append(dayAndTime);
 
 function createTimeBlocks() {
     var blocks = "";
-    // for (var i = 0; i < workHours.length; i++) {
-    //     var hourBlock = "" +
-    //         '<div class="input-group mb-3">' +
-    //         '<div class="input-group-prepend">' +
-    //         '<span class="input-group-text">' + workHours[i] + '</span>' +
-    //         '</div>' +
-    //         '<input type="text" class="form-control" id="input-' + workHours[i] + '">' +
-    //         '<div class="input-group-append">' +
-    //         '<button class="btn btn-primary save-button" data-time="'  + workHours[i] + '"><i class="fa fa-save"></i></button>' +
-    //         '</div>' +
-    //         '</div>';
-    //     blocks += hourBlock;
-    // }
+   
 
     function setColor(time) {
       if (time === currentSlot) {
@@ -45,7 +32,6 @@ function createTimeBlocks() {
           '<div class="input-group-prepend">' +
           '<span class="input-group-text">' + time + '</span>' +
           '</div>' +
-          // '<input type="text" class="form-control" id="input-' + time + '">' + savedTasks[time] + '</input>' +
           `<input type="text" style="background-color: ${setColor(time)}" class="form-control" id="input-${time}" value="${savedTasks[time]}"/>` +
           '<div class="input-group-append">' +
           '<button class="btn btn-primary save-button" data-time="'  + time + '"><i class="fa fa-save"></i></button>' +
@@ -86,3 +72,21 @@ createTimeBlocks();
     // use for loop to add input attribute to timeblocks
 // Clicking each block allows the user to add text.
 // Clicking a save button/icon will save the content to localStorage.
+
+ // for (var i = 0; i < workHours.length; i++) {
+    //     var hourBlock = "" +
+    //         '<div class="input-group mb-3">' +
+    //         '<div class="input-group-prepend">' +
+    //         '<span class="input-group-text">' + workHours[i] + '</span>' +
+    //         '</div>' +
+    //         '<input type="text" class="form-control" id="input-' + workHours[i] + '">' +
+    //         '<div class="input-group-append">' +
+    //         '<button class="btn btn-primary save-button" data-time="'  + workHours[i] + '"><i class="fa fa-save"></i></button>' +
+    //         '</div>' +
+    //         '</div>';
+    //     blocks += hourBlock;
+    // }
+
+    // var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+    // '<input type="text" class="form-control" id="input-' + time + '">' + savedTasks[time] + '</input>' +

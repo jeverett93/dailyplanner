f015ed4d8f9b2c7cada91e94a100be992d085837
Josh Everett <josheverett93@gmail.com>
2020-03-10 12:46:38 -0500

    changing timeblocks back

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 // Getting current time to pop up
 var dayAndTime = moment().format('LLLL');
 var container = $("#container");
-var workData = {"9": "", "10": "", "11": "", "12": "", "13": "", "14": "", "15": "", "16": "", "17": ""}[moment().hour()];
+var workData = {"9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": ""};
 var savedTasks = JSON.parse(localStorage.getItem("tasks")) || workData;
 
-// var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
+var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
 
 $("#currentDay").append(dayAndTime);
 
@@ -15,13 +15,13 @@ function createTimeBlocks() {
    
 
     function setColor(time) {
-      if (time === workData) {
+      if (time === currentSlot) {
         return 'lavender'
       }
-      if (time > workData) {
+      if (time > currentSlot) {
         return 'pink'
       }
-      if (time < workData) {
+      if (time < currentSlot) {
         return 'aqua'
       }
     }

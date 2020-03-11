c5a03eadf6d3bfa11357c2c3016444ec9ad8994c
Josh Everett <josheverett93@gmail.com>
2020-03-11 11:00:51 -0500

    hour 17 input is clear when the value is null

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var container = $("#container");
 var workData = {"9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": ""};
 var savedTasks = JSON.parse(localStorage.getItem("tasks")) || workData;
 
-// var currentSlot = {9: "9AM", 10: "10AM", 11: "11AM", 12: "12PM", 13: "1PM", 14: "2PM", 15: "3PM", 16: "4PM", 17: "5PM"}[moment().hour()]
+
 
 var currentHour = moment().hour();
 var currentSlot = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
@@ -27,7 +27,14 @@ function createTimeBlocks() {
           '<button class="btn btn-primary save-button" data-time="'  + time + '"><i class="fa fa-save"></i></button>' +
           '</div>' +
           '</div>';
+          
       blocks += hourBlock;
+
+      if (savedTasks[time] == null){
+        savedTasks.push("");
+      };
+
+      console.log(hourBlock);
 
     });
 
@@ -37,8 +44,6 @@ function createTimeBlocks() {
         var timeSlot = event.currentTarget.getAttribute('data-time')
 
         var userTask = $('#input-' + timeSlot).val();
-        console.log(userTask);
-        // savedTasks.push(userTask);
         savedTasks[timeSlot] = userTask
         localStorage.setItem("tasks", JSON.stringify(savedTasks));
     });
@@ -50,8 +55,8 @@ function createTimeBlocks() {
 createTimeBlocks();
 
 for (var i = 0; i < currentSlot.length; i++) {
-  console.log('currentHour', currentHour)
-  console.log('currentSlot', parseInt(currentSlot[i]))
+  // console.log('currentHour', currentHour)
+  // console.log('currentSlot', parseInt(currentSlot[i]))
 
   if (currentHour === parseInt(currentSlot[i])) {
     $("#input-" + currentSlot[i]).attr('style', 'background-color: lightgreen;')

47a24db4d6011b30795a167ad85a9a7fbda744e3
Josh Everett <josheverett93@gmail.com>
2020-03-11 15:29:50 -0500

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,17 @@ var container = $("#container");
 var workData = {"9AM": "", "10AM": "", "11AM": "", "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": ""};
 var savedTasks = JSON.parse(localStorage.getItem("tasks")) || workData;
 
-
-
+// variables for current moment and times to compare it to
 var currentHour = moment().hour();
 var currentSlot = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
 
 $("#currentDay").append(dayAndTime);
 
+// function to create time blocks
 function createTimeBlocks() {
     var blocks = "";
 
+    // Makes all time blocks empty inputs with save buttons
     currentSlot.forEach(function(time) {
       var hourBlock = "" +
           '<div class="input-group mb-3">' +
@@ -30,12 +31,11 @@ function createTimeBlocks() {
           
       blocks += hourBlock;
 
-      console.log(hourBlock);
-
     });
 
     container.append(blocks);
 
+    // click event that saves events entered by users and places them into local storage
     $(".save-button").on("click", function(event) {
         var timeSlot = event.currentTarget.getAttribute('data-time')
 
@@ -50,9 +50,9 @@ function createTimeBlocks() {
 
 createTimeBlocks();
 
+// color codes time blocks based on its relationship to current time
 for (var i = 0; i < currentSlot.length; i++) {
-  // console.log('currentHour', currentHour)
-  // console.log('currentSlot', parseInt(currentSlot[i]))
+  
 
   if (currentHour === parseInt(currentSlot[i])) {
     $("#input-" + currentSlot[i]).attr('style', 'background-color: lightgreen;')
